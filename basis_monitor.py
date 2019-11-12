74338ad3f90b6a84af5088336787726f5c5df945
huang <hhk0@outlook.com>
2019-11-12 09:31:15 +0800

    Update

--- basis_monitor.py
+++ basis_monitor.py
@@ -49,18 +49,28 @@ def monitor():
 
             future_price = tsl.getCurrentPrice("IC1911")
             index_price = tsl.getCurrentPrice("SH000905")
+            long_future_price = tsl.getCurrentPrice("IC1912")
             try:
-                base = future_price - index_price
+                basis = future_price - index_price
+                calendar_spread = long_future_price - future_price
             except TypeError:
                 ax.texts = list()
                 ax.text(0.02, 0.5,"Failed to get TSL data! Try again in 5 seconds.")
                 plt.draw()
                 time.sleep(5)
                 continue
-            if base <= val_min or base >= val_max:
+            if basis <= val_min or basis >= val_max:
                 for i in range(50):
                     ax.texts = list()
-                    ax.text(0 + random.random()/5, 0.2, str(int(base)), fontdict={"color": "red", "fontsize": 180})
+                    ax.text(0 + random.random()/5, 0.2, str(int(basis)), fontdict={"color": "red", "fontsize": 180})
+                    plt.draw()
+                    time.sleep(0.1)
+                stop_func(None)
+                ax.set_title("Suspend")
+            if calendar_spread <= val_min2 or calendar_spread >= val_max2:
+                for i in range(50):
+                    ax.texts = list()
+                    ax.text(0 + random.random()/5, 0.2, str(int(calendar_spread)), fontdict={"color": "green", "fontsize": 180})
                     plt.draw()
                     time.sleep(0.1)
                 stop_func(None)
@@ -90,24 +100,42 @@ def update_max(val):
     val_max = val
     print("Set max: ", val_max)
 
+def update_min2(val):
+    global val_min2
+    val_min2 = val
+    print("Set min2: ", val_min2)
+
+def update_max2(val):
+    global val_max2
+    val_max2 = val
+    print("Set max2: ", val_max2)
+
 
 if __name__ == "__main__":
     stop = False
     val_min = -60
     val_max = 10
+    val_min2 = -90
+    val_max2 = 10
     fig = plt.figure(figsize=(5,5))
     ax = fig.add_subplot(1, 1, 1)
-    plt.subplots_adjust(bottom=0.35)
+    plt.subplots_adjust(bottom=0.4)
     axnext = plt.axes([0.78, 0.05, 0.2, 0.075])
     bnext = Button(axnext, 'Resume/Stop')
     bnext.on_clicked(stop_func)
-    left, bottom, width, height = 0.15, 0.05, 0.5, 0.1
-    slider_ax_min = plt.axes([left, bottom, width, height])
-    slider_min = Slider(slider_ax_min, 'Min', valmin=-60, valmax=10, valstep=1, valinit=-60)
-    slider_min.on_changed(update_min)
-    slider_ax_max = plt.axes([left, bottom+ 0.15, width, height])
+    left, bottom, width, height = 0.15, 0.05, 0.5, 0.05
+    slider_ax_max = plt.axes([left, bottom + 0.24, width, height])
     slider_max = Slider(slider_ax_max, 'Max', valmin=-60, valmax=10, valstep=1, valinit=10)
     slider_max.on_changed(update_max)
+    slider_ax_min = plt.axes([left, bottom + 0.16, width, height])
+    slider_min = Slider(slider_ax_min, 'Min', valmin=-60, valmax=10, valstep=1, valinit=-60)
+    slider_min.on_changed(update_min)
+    slider_ax_max2 = plt.axes([left, bottom + 0.08, width, height])
+    slider_max2 = Slider(slider_ax_max2, 'Max', valmin=-90, valmax=10, valstep=1, valinit=10)
+    slider_max2.on_changed(update_max2)
+    slider_ax_min2 = plt.axes([left, bottom, width, height])
+    slider_min2 = Slider(slider_ax_min2, 'Min', valmin=-90, valmax=10, valstep=1, valinit=-90)
+    slider_min2.on_changed(update_min2)
     ax.set_title("Working")
     t = Thread(target=monitor)
     t.start()
